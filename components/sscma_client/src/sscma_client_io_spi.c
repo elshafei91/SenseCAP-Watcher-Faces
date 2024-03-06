f751452c22ca7f046fee849cc01e7aeba9d93ca7
LynnL4 <lht856@foxmail.com>
2024-03-06 19:53:47 +0800

    feat: add lock for sscma client io

--- components/sscma_client/src/sscma_client_io_spi.c
+++ components/sscma_client/src/sscma_client_io_spi.c
@@ -2,8 +2,14 @@
 #include <string.h>
 #include <sys/cdefs.h>
 #include "sdkconfig.h"
+#if CONFIG_SSCMA_ENABLE_DEBUG_LOG
+// The local log level must be defined before including esp_log.h
+// Set the maximum log level for this source file
+#define LOG_LOCAL_LEVEL ESP_LOG_DEBUG
+#endif
 #include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "cJSON.h"
 #include "sscma_client_io_interface.h"
 #include "sscma_client_io.h"
 #include "driver/spi_master.h"
@@ -42,11 +48,15 @@ typedef struct
     size_t spi_trans_max_bytes;  // SPI transaction max bytes
     int wait_delay;              // SPI wait delay
     void *user_ctx;              // User context
+    SemaphoreHandle_t lock;      // Lock
     uint8_t buffer[PACKET_SIZE];
 } sscma_client_io_spi_t;
 
 esp_err_t sscma_client_new_io_spi_bus(sscma_client_spi_bus_handle_t bus, sscma_client_io_spi_config_t *io_config, sscma_client_io_handle_t *ret_io)
 {
+#if CONFIG_SSCMA_ENABLE_DEBUG_LOG
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+#endif
     esp_err_t ret = ESP_OK;
     sscma_client_io_spi_t *spi_client_io = NULL;
     ESP_GOTO_ON_FALSE(io_config && ret_io, ESP_ERR_INVALID_ARG, err, TAG, "invalid argument");
@@ -86,6 +96,9 @@ esp_err_t sscma_client_new_io_spi_bus(sscma_client_spi_bus_handle_t bus, sscma_c
     spi_client_io->base.read = client_io_spi_read;
     spi_client_io->base.available = client_io_spi_available;
 
+    spi_client_io->lock = xSemaphoreCreateMutex();
+    ESP_GOTO_ON_FALSE(spi_client_io->lock, ESP_ERR_NO_MEM, err, TAG, "no mem for mutex");
+
     size_t max_trans_bytes = 0;
     ESP_GOTO_ON_ERROR(spi_bus_get_max_transaction_len((spi_host_device_t)bus, &max_trans_bytes), err, TAG, "get spi max transaction len failed");
     spi_client_io->spi_trans_max_bytes = max_trans_bytes;
@@ -98,6 +111,15 @@ esp_err_t sscma_client_new_io_spi_bus(sscma_client_spi_bus_handle_t bus, sscma_c
 err:
     if (spi_client_io)
     {
+        if (spi_client_io->spi_dev)
+        {
+            spi_bus_remove_device(spi_client_io->spi_dev);
+            spi_bus_free((spi_host_device_t)bus);
+        }
+        if (spi_client_io->lock)
+        {
+            vSemaphoreDelete(spi_client_io->lock);
+        }
         free(spi_client_io);
     }
     return ret;
@@ -107,8 +129,12 @@ static esp_err_t client_io_spi_del(sscma_client_io_t *io)
 {
     esp_err_t ret = ESP_OK;
     sscma_client_io_spi_t *spi_client_io = __containerof(io, sscma_client_io_spi_t, base);
-
+    if (spi_client_io->lock)
+    {
+        vSemaphoreDelete(spi_client_io->lock);
+    }
     spi_bus_remove_device(spi_client_io->spi_dev);
+    spi_bus_free((spi_host_device_t)spi_client_io->spi_dev);
     if (spi_client_io->sync_gpio_num >= 0)
     {
         gpio_reset_pin(spi_client_io->sync_gpio_num);
@@ -128,7 +154,13 @@ static esp_err_t client_io_spi_write(sscma_client_io_t *io, const void *data, si
     uint16_t remain = len % MAX_PL_LEN;
     size_t trans_len = 0;
 
-    ESP_RETURN_ON_ERROR(spi_device_acquire_bus(spi_client_io->spi_dev, portMAX_DELAY), TAG, "acquire spi bus failed");
+    xSemaphoreTake(spi_client_io->lock, portMAX_DELAY);
+
+    if (spi_device_acquire_bus(spi_client_io->spi_dev, portMAX_DELAY) != ESP_OK)
+    {
+        xSemaphoreGive(spi_client_io->lock);
+        return ESP_FAIL;
+    }
 
     if (data)
     {
@@ -216,6 +248,7 @@ static esp_err_t client_io_spi_write(sscma_client_io_t *io, const void *data, si
 
 err:
     spi_device_release_bus(spi_client_io->spi_dev);
+    xSemaphoreGive(spi_client_io->lock);
     return ret;
 }
 
@@ -228,7 +261,13 @@ static esp_err_t client_io_spi_read(sscma_client_io_t *io, void *data, size_t le
     uint16_t remain = len % MAX_RECIEVE_SIZE;
     size_t trans_len = 0;
 
-    ESP_RETURN_ON_ERROR(spi_device_acquire_bus(spi_client_io->spi_dev, portMAX_DELAY), TAG, "acquire spi bus failed");
+    xSemaphoreTake(spi_client_io->lock, portMAX_DELAY);
+
+    if (spi_device_acquire_bus(spi_client_io->spi_dev, portMAX_DELAY) != ESP_OK)
+    {
+        xSemaphoreGive(spi_client_io->lock);
+        return ESP_FAIL;
+    }
 
     if (data)
     {
@@ -371,6 +410,7 @@ static esp_err_t client_io_spi_read(sscma_client_io_t *io, void *data, size_t le
 
 err:
     spi_device_release_bus(spi_client_io->spi_dev);
+    xSemaphoreGive(spi_client_io->lock);
     return ret;
 }
 
@@ -382,13 +422,20 @@ static esp_err_t client_io_spi_available(sscma_client_io_t *io, size_t *len)
     sscma_client_io_spi_t *spi_client_io = __containerof(io, sscma_client_io_spi_t, base);
     size_t trans_len = 0;
 
-    ESP_RETURN_ON_ERROR(spi_device_acquire_bus(spi_client_io->spi_dev, portMAX_DELAY), TAG, "acquire spi bus failed");
-
     *len = 0;
+
+    xSemaphoreTake(spi_client_io->lock, portMAX_DELAY);
 
     if (gpio_get_level(spi_client_io->sync_gpio_num) == 0)
     {
-        goto err;
+        xSemaphoreGive(spi_client_io->lock);
+        return ESP_OK;
+    }
+
+    if (spi_device_acquire_bus(spi_client_io->spi_dev, portMAX_DELAY) != ESP_OK)
+    {
+        xSemaphoreGive(spi_client_io->lock);
+        return ESP_FAIL;
     }
 
     trans_len = PACKET_SIZE;
@@ -443,5 +490,6 @@ static esp_err_t client_io_spi_available(sscma_client_io_t *io, size_t *len)
     *len = (spi_client_io->buffer[0] << 8) | spi_client_io->buffer[1];
 err:
     spi_device_release_bus(spi_client_io->spi_dev);
+    xSemaphoreGive(spi_client_io->lock);
     return ret;
 }
--- components/sscma_client/src/sscma_client_io_uart.c
+++ components/sscma_client/src/sscma_client_io_uart.c
@@ -3,6 +3,9 @@
 #include <sys/cdefs.h>
 #include "sdkconfig.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/semphr.h"
+
 #include "sscma_client_io_interface.h"
 #include "sscma_client_io.h"
 #include "driver/uart.h"
@@ -20,13 +23,16 @@ static esp_err_t client_io_uart_available(sscma_client_io_t *io, size_t *len);
 typedef struct
 {
     sscma_client_io_t base;
-    uint32_t uart_port; // UART port
-    void *user_ctx;     // User context
+    SemaphoreHandle_t lock; // Mutex lock
+    uint32_t uart_port;     // UART port
+    void *user_ctx;         // User context
 } sscma_client_io_uart_t;
-
 
 esp_err_t sscma_client_new_io_uart_bus(sscma_client_uart_bus_handle_t bus, sscma_client_io_uart_config_t *io_config, sscma_client_io_handle_t *ret_io)
 {
+#if CONFIG_SSCMA_ENABLE_DEBUG_LOG
+    esp_log_level_set(TAG, ESP_LOG_DEBUG);
+#endif
     esp_err_t ret = ESP_OK;
     sscma_client_io_uart_t *uart_client_io = NULL;
     ESP_GOTO_ON_FALSE(io_config && ret_io, ESP_ERR_INVALID_ARG, err, TAG, "invalid argument");
@@ -41,6 +47,9 @@ esp_err_t sscma_client_new_io_uart_bus(sscma_client_uart_bus_handle_t bus, sscma
     uart_client_io->base.read = client_io_uart_read;
     uart_client_io->base.available = client_io_uart_available;
 
+    uart_client_io->lock = xSemaphoreCreateMutex();
+    ESP_GOTO_ON_FALSE(uart_client_io->lock, ESP_ERR_NO_MEM, err, TAG, "no mem for mutex");
+
     *ret_io = &uart_client_io->base;
     ESP_LOGI(TAG, "new uart sscma client io @%p", uart_client_io);
 
@@ -49,8 +58,13 @@ esp_err_t sscma_client_new_io_uart_bus(sscma_client_uart_bus_handle_t bus, sscma
 err:
     if (uart_client_io)
     {
+        if (uart_client_io->lock)
+        {
+            vSemaphoreDelete(uart_client_io->lock);
+        }
         free(uart_client_io);
     }
+
     return ret;
 }
 
@@ -59,8 +73,14 @@ esp_err_t client_io_uart_del(sscma_client_io_t *io)
     esp_err_t ret = ESP_OK;
     sscma_client_io_uart_t *uart_client_io = __containerof(io, sscma_client_io_uart_t, base);
 
-    ESP_LOGD(TAG, "del uart sscma client io @%p", uart_client_io);
+    if (uart_client_io->lock)
+    {
+        vSemaphoreDelete(uart_client_io->lock);
+    }
+
     free(uart_client_io);
+
+    ESP_LOGD(TAG, "del uart sscma client io @%p", uart_client_io);
     return ret;
 }
 
@@ -70,7 +90,11 @@ static esp_err_t client_io_uart_write(sscma_client_io_t *io, const void *data, s
     sscma_client_io_uart_t *uart_client_io = __containerof(io, sscma_client_io_uart_t, base);
     ESP_GOTO_ON_FALSE(uart_client_io, ESP_ERR_INVALID_STATE, err, TAG, "uart not initialized");
 
+    xSemaphoreTake(uart_client_io->lock, portMAX_DELAY);
+
     ret = uart_write_bytes(uart_client_io->uart_port, data, len);
+
+    xSemaphoreGive(uart_client_io->lock);
 
 err:
     return ret == len ? ESP_OK : ESP_FAIL;
@@ -82,7 +106,9 @@ static esp_err_t client_io_uart_read(sscma_client_io_t *io, void *data, size_t l
     sscma_client_io_uart_t *uart_client_io = __containerof(io, sscma_client_io_uart_t, base);
     ESP_GOTO_ON_FALSE(uart_client_io, ESP_ERR_INVALID_STATE, err, TAG, "uart not initialized");
 
+    xSemaphoreTake(uart_client_io->lock, portMAX_DELAY);
     ret = uart_read_bytes(uart_client_io->uart_port, data, len, portMAX_DELAY);
+    xSemaphoreGive(uart_client_io->lock);
 
 err:
     return ret == len ? ESP_OK : ESP_FAIL;
@@ -94,12 +120,15 @@ static esp_err_t client_io_uart_available(sscma_client_io_t *io, size_t *len)
     sscma_client_io_uart_t *uart_client_io = __containerof(io, sscma_client_io_uart_t, base);
     ESP_GOTO_ON_FALSE(uart_client_io, ESP_ERR_INVALID_STATE, err, TAG, "uart not initialized");
 
+    xSemaphoreTake(uart_client_io->lock, portMAX_DELAY);
     ret = uart_get_buffered_data_len(uart_client_io->uart_port, len);
 
     if (ret != ESP_OK)
     {
         *len = 0;
     }
+
+    xSemaphoreGive(uart_client_io->lock);
 
 err:
     return ret;
