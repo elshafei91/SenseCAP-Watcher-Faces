2b4f9f5e2d214beb881d7f186a3d3ad6be006aa3
LynnL4 <lht856@foxmail.com>
2024-04-04 15:39:37 +0800

    fix: [sscma_client] the process needs to be hung during reset

--- components/sscma_client/src/sscma_client_io.c
+++ components/sscma_client/src/sscma_client_io.c
@@ -13,12 +13,16 @@ esp_err_t sscma_client_io_del(sscma_client_io_t *io)
 esp_err_t sscma_client_io_write(sscma_client_io_t *io, const void *data, size_t len)
 {
     ESP_RETURN_ON_FALSE(io, ESP_ERR_INVALID_ARG, TAG, "invalid argument");
+    assert(len > 0);
+    assert(data);
     return io->write(io, data, len);
 }
 
 esp_err_t sscma_client_io_read(sscma_client_io_t *io, void *data, size_t len)
 {
     ESP_RETURN_ON_FALSE(io, ESP_ERR_INVALID_ARG, TAG, "invalid argument");
+    assert(len > 0);
+    assert(data);
     return io->read(io, data, len);
 }
 
