be76fe51abbccbd840fa1a6abec9725541053205
QingWind6 <993628705@qq.com>
2024-05-11 16:21:36 +0800

    feat: [factory_fw] flash volume and brightness value into nvs

--- examples/factory_firmware/main/ui/screens/ui_Page_Vir.c
+++ examples/factory_firmware/main/ui/screens/ui_Page_Vir.c
@@ -10,6 +10,12 @@ void ui_Page_Vir_screen_init(void)
     ui_Page_Vir = lv_obj_create(NULL);
     lv_obj_clear_flag(ui_Page_Vir, LV_OBJ_FLAG_SCROLLABLE);      /// Flags
 
+    // lv_obj_t * img = lv_img_create(ui_Page_Vir);
+    // /* Assuming a File system is attached to letter 'A'
+    //  * E.g. set LV_USE_FS_STDIO 'A' in lv_conf.h */
+    // lv_img_set_src(img, "/spiffs/otherface1.png");
+    // lv_obj_align(img, LV_ALIGN_RIGHT_MID, -20, 0);
+
     lv_obj_add_event_cb(ui_Page_Vir, ui_event_Page_Vir, LV_EVENT_ALL, NULL);
 
 }
