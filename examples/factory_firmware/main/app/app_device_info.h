bef9672a7d2f27476a1055ed5cb2f2b233ce8784
Dylanliacc <dylan3000dylan@gmail.com>
2024-05-16 16:16:42 +0800

    feat: [factory_fw] add brightness deviceinfo basic layer [WIP]

--- examples/factory_firmware/main/app/app_device_info.h
+++ examples/factory_firmware/main/app/app_device_info.h
@@ -8,5 +8,6 @@ uint8_t * get_sn(int caller);
 void app_device_info_init();
 char* get_software_version(int caller);
 char* get_himax_software_version(int caller); 
-
+uint8_t *get_brightness(int caller);
+uint8_t *set_brightness(int caller, int value);
 #endif // APP_DEVICEINFO_H
