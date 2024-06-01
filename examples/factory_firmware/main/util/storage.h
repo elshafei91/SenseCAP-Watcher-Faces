e729306124d93e75b2230b38ee92d796a829e045
virgil <virgil.wang.cj@gmail.com>
2024-06-01 15:44:32 +0800

    feat:[factory_fw] read and write spiffs via event loop.

--- examples/factory_firmware/main/util/storage.h
+++ examples/factory_firmware/main/util/storage.h
@@ -16,10 +16,15 @@ int storage_init(void);
 
 esp_err_t storage_write(char *p_key, void *p_data, size_t len);
 
-
 //p_len : inout
 esp_err_t storage_read(char *p_key, void *p_data, size_t *p_len);
 esp_err_t storage_erase();
+
+//eg: file: /spiffs/test.text
+esp_err_t storage_file_write(char *file, void *p_data, size_t len);
+esp_err_t storage_file_read(char *file, void *p_data, size_t *p_len); //p_len : inout
+esp_err_t storage_file_size_get(char *file, size_t *p_len);
+
 #ifdef __cplusplus
 }
 #endif
