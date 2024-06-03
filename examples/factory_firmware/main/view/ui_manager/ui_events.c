65e94a3477546bccd01b88382789fec63b853099
QingWind6 <993628705@qq.com>
2024-06-03 16:23:34 +0800

    chore: [factory_fw] modify wifi ssid label set

--- examples/factory_firmware/main/view/ui_manager/ui_events.c
+++ examples/factory_firmware/main/view/ui_manager/ui_events.c
@@ -932,19 +932,15 @@ void setwific_cb(lv_event_t *e)
     ssid_string[sizeof(ssid_string) - 1] = '\0';
     lv_label_set_text(ui_wifissid, ssid_string);
     // binded
-    if (wifi_page_id)
+    lv_pm_open_page(g_main, NULL, PM_CLEAR_GROUP, &ui_Page_Wifi, LV_SCR_LOAD_ANIM_FADE_ON, 100, 0, &ui_Page_Wifi_screen_init);
+    lv_obj_clear_flag(ui_wifip1, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_move_foreground(ui_wifiicon);
+    lv_obj_add_flag(ui_wifip2, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_add_flag(ui_wifip3, LV_OBJ_FLAG_HIDDEN);
+
+    if (!wifi_page_id)
     {
-        lv_pm_open_page(g_main, NULL, PM_CLEAR_GROUP, &ui_Page_Wifi, LV_SCR_LOAD_ANIM_FADE_ON, 100, 0, &ui_Page_Wifi_screen_init);
-        lv_obj_clear_flag(ui_wifip1, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_move_foreground(ui_wifiicon);
-        lv_obj_add_flag(ui_wifip2, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(ui_wifip3, LV_OBJ_FLAG_HIDDEN);
-    }
-    else
-    {
-        // unbinded
-        lv_pm_open_page(g_main, NULL, PM_CLEAR_GROUP, &ui_Page_Connect, LV_SCR_LOAD_ANIM_FADE_ON, 100, 0, &ui_Page_Connect_screen_init);
-        Page_ConnAPP_Mate();
+        lv_label_set_text(ui_wifissid, "None");
     }
 }
 
