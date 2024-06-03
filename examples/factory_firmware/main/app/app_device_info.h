2d4e32f61dac96f5d807d1500d14153153181e49
Jack Shao <jacky.shaoxg@gmail.com>
2024-06-03 23:00:42 +0800

    chore: refactor the app_device_info.c/h, fix a few bugs

--- examples/factory_firmware/main/app/app_device_info.h
+++ examples/factory_firmware/main/app/app_device_info.h
@@ -9,25 +9,29 @@
 enum { BLE_CONNECTED, BLE_DISCONNECTED, STATUS_WAITTING };
 
 uint8_t *get_sn(int caller);
+uint8_t *get_bt_mac();
+uint8_t *get_sn_code();
+uint8_t *get_eui();
 char *get_software_version(int caller);
 char *get_himax_software_version(int caller);
 
-ai_service_pack *get_ai_service(int caller);
-int get_time_automatic(int caller);
-uint8_t *set_time_automatic(int caller, int value);
+int get_brightness(int caller);
+esp_err_t set_brightness(int caller, int value);
+int get_rgb_switch(int caller);
+esp_err_t set_rgb_switch(int caller, int value);
+int get_sound(int caller);
+esp_err_t set_sound(int caller, int value);
 int get_cloud_service_switch(int caller);
 esp_err_t set_cloud_service_switch(int caller, int value);
-int get_brightness(int caller);
-uint8_t *set_brightness(int caller, int value);
-uint8_t *get_sound(int caller);
-uint8_t *set_sound(int caller, int value);
-uint8_t *set_rgb_switch(int caller, int value);
-int *get_rgb_switch(int caller);
-int *get_reset_factory(int caller);
-uint8_t *set_reset_factory(int caller, int value);
-uint8_t *get_bt_mac();
-uint8_t *get_eui();
-uint8_t *get_sn_code();
+int get_reset_factory(int caller);
+esp_err_t set_reset_factory(int caller, int value);
+
+/**
+ * will be deprecated!
+*/
+int get_time_automatic(int caller);
+esp_err_t set_time_automatic(int caller, int value);
+
 /**
  * all the following size unit is KiB.
 */
