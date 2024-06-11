9cfba74a4dcd2f49ffbe5b65944c27e4618fb0c4
QingWind6 <993628705@qq.com>
2024-06-11 14:06:41 +0800

    fix: [factory_fw] fix group error in OTA progress

--- examples/factory_firmware/main/view/view.c
+++ examples/factory_firmware/main/view/view.c
@@ -10,6 +10,7 @@
 #include "app_png.h"
 #include "ui_manager/pm.h"
 #include "ui_manager/animation.h"
+#include "ui_manager/event.h"
 
 #define PNG_IMG_NUMS 24
 
@@ -37,6 +38,12 @@ extern lv_img_dsc_t *g_anaylze_img_dsc[MAX_IMAGES];
 extern lv_img_dsc_t *g_standby_img_dsc[MAX_IMAGES];
 extern lv_img_dsc_t *g_greet_img_dsc[MAX_IMAGES];
 extern lv_img_dsc_t *g_detected_img_dsc[MAX_IMAGES];
+
+// view_alarm obj extern
+extern lv_obj_t * ui_viewavap;
+extern lv_obj_t * ui_avat1;
+extern lv_obj_t * ui_avabtn1;
+extern lv_obj_t * ui_avabtn2;
 
 extern int g_detect_image_count;
 extern int g_speak_image_count;
@@ -331,7 +338,6 @@ static void __view_event_handler(void* handler_args, esp_event_base_t base, int3
             case VIEW_EVENT_TASK_FLOW_STOP:{
                 ESP_LOGI(TAG, "event: VIEW_EVENT_TASK_FLOW_STOP");
                 task_down = 1;
-                lv_obj_add_flag(ui_viewlivp, LV_OBJ_FLAG_HIDDEN);
                 lv_obj_add_flag(ui_viewavap, LV_OBJ_FLAG_HIDDEN);
                 // event_post_to
                 esp_event_post_to(app_event_loop_handle, VIEW_EVENT_BASE, VIEW_EVENT_ALARM_OFF, &task_down, sizeof(uint8_t), portMAX_DELAY);
@@ -364,12 +370,6 @@ static void __view_event_handler(void* handler_args, esp_event_base_t base, int3
                 lv_label_set_text(ui_taskerrt2, error_msg);
                 break;
             }
-            //Todo
-            // case VIEW_EVENT_BAT_DRAIN_SHUTDOWN:{
-
-            //     break;
-            // }
-
             default:
                 break;
         }
@@ -379,13 +379,17 @@ static void __view_event_handler(void* handler_args, esp_event_base_t base, int3
         {
             case CTRL_EVENT_OTA_AI_MODEL:{
                 ESP_LOGI(TAG, "event: CTRL_EVENT_OTA_AI_MODEL");
-                _ui_screen_change(&ui_Page_CurTask2, LV_SCR_LOAD_ANIM_FADE_ON, 100, 0, &ui_Page_CurTask2_screen_init);
+                if(lv_scr_act() != ui_Page_CurTask2)
+                {
+                    lv_group_remove_all_objs(g_main);
+                    _ui_screen_change(&ui_Page_CurTask2, LV_SCR_LOAD_ANIM_FADE_ON, 100, 0, &ui_Page_CurTask2_screen_init);
+                }
                 struct view_data_ota_status * ota_st = (struct view_data_ota_status *)event_data;
                 lv_obj_add_flag(ui_otaicon, LV_OBJ_FLAG_HIDDEN);
                 if(ota_st->status == 0)
                 {
                     ESP_LOGI(TAG, "OTA download succeeded");
-                    _ui_screen_change(&ui_Page_ViewAva, LV_SCR_LOAD_ANIM_FADE_ON, 100, 0, &ui_Page_ViewAva_screen_init);
+                    lv_pm_open_page(g_main, &group_page_view, PM_ADD_OBJS_TO_GROUP, &ui_Page_ViewAva, LV_SCR_LOAD_ANIM_FADE_ON, 100, 3000, &ui_Page_ViewAva_screen_init);
                 }else if (ota_st->status == 1)
                 {
                     lv_obj_clear_flag(ui_otaspinner, LV_OBJ_FLAG_HIDDEN);
