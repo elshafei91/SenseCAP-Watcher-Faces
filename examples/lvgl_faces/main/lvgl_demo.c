3967a3ea6788d7aec35a9279fcd7a4e9fa5a97ac
elshafei91 <mahmoud.alshafeei@gmail.com>
2025-05-31 20:10:32 +0200

    Testing a demo face

---
+++ examples/lvgl_faces/main/lvgl_demo.c
@@ -0,0 +1,51 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "esp_err.h"
+#include "esp_log.h"
+#include "esp_check.h"
+
+#include "sensecap-watcher.h"
+
+// #include "lv_demos.h"
+#include "ui/happy_face.c"
+
+
+
+static const char *TAG = "LVGL_DEMO";
+
+static lv_disp_t *lvgl_disp = NULL;
+static esp_io_expander_handle_t io_expander = NULL;
+
+void app_main(void)
+{
+    io_expander = bsp_io_expander_init();
+    assert(io_expander != NULL);
+    lvgl_disp = bsp_lvgl_init();
+    assert(lvgl_disp != NULL);
+    
+    
+    // Lock the mutex due to the LVGL APIs are not thread-safe
+    if (lvgl_port_lock(0))
+    {
+        // lv_demo_widgets(); /* A widgets example */
+        lv_obj_t *img = lv_img_create(lv_scr_act());
+        lv_img_set_src(img, &happy_face);
+        lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+        // lv_demo_music(); /* A modern, smartphone-like music player demo. */
+        // lv_demo_stress();       /* A stress test for LVGL. */
+        // lv_demo_benchmark();    /* A demo to measure the performance of LVGL or to compare different settings. */
+
+        // Release the mutex
+        lvgl_port_unlock();
+    }
+
+    while (1)
+    {
+        printf("free_heap_size = %ld\n", esp_get_free_heap_size());
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
---
+++ examples/lvgl_faces/main/ui/happy_face.h
@@ -0,0 +1,8 @@
+#ifndef HAPPY_FACE_H
+#define HAPPY_FACE_H
+
+#include "lvgl.h"
+
+LV_IMG_DECLARE(happy_face);
+
+#endif // HAPPY_FACE_H
