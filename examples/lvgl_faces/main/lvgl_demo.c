f24600e57c536a432aacf2b081c29517ad06211d
elshafei91 <mahmoud.alshafeei@gmail.com>
2025-06-05 09:41:21 +0200

    audio implementation for testing

--- examples/lvgl_faces/main/lvgl_demo.c
+++ examples/lvgl_faces/main/lvgl_demo.c
@@ -7,16 +7,13 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "esp_check.h"
+#include <dirent.h>
 
 #include "sensecap-watcher.h"
 
 // #include "lv_demos.h"
 #include "ui/neutral_face.c"
-
-
-#include <stdio.h>
-#include <inttypes.h>
-#include <sdkconfig.h>
+#include "ui/happy_face.c"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/queue.h"
@@ -28,11 +25,10 @@
 #include "model_path.h"
 #include "string.h"
 
-#define BUFFER_SIZE      (1024)
-#define SAMPLE_RATE      (16000) // For recording
-#define SAMPLE_CHANNELS  (1)
-#define DEFAULT_VOLUME   (100)
-#define RECORDING_LENGTH (300)
+#define BUFFER_SIZE     (1024)
+#define SAMPLE_RATE     (16000) // For recording
+#define DEFAULT_VOLUME  (100)
+
 typedef struct __attribute__((packed))
 {
     uint8_t ignore_0[22];
@@ -66,23 +62,30 @@ static void btn_handler(void *button_handle, void *usr_data)
 
 static void audio_task(void *arg)
 {
-    bsp_codec_init();
+    esp_err_t err = bsp_codec_init();
+    if (err != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Codec init failed: %d", err);
+        vTaskDelete(NULL);
+    }
     bsp_codec_volume_set(DEFAULT_VOLUME, NULL);
 
-    const char *play_filename = "/sdcard/Neutral.wav";
+    const char *play_filename = "/sdcard/NEUTRAL.WAV";
 
     int16_t *wav_bytes = heap_caps_malloc(BUFFER_SIZE, MALLOC_CAP_DEFAULT);
     assert(wav_bytes != NULL);
 
     FILE *play_file = fopen(play_filename, "rb");
-    if (!play_file) {
+    if (!play_file)
+    {
         ESP_LOGE(TAG, "Failed to open %s", play_filename);
         vTaskDelete(NULL);
         return;
     }
 
     dumb_wav_header_t wav_header;
-    if (fread((void *)&wav_header, 1, sizeof(wav_header), play_file) != sizeof(wav_header)) {
+    if (fread((void *)&wav_header, 1, sizeof(wav_header), play_file) != sizeof(wav_header))
+    {
         ESP_LOGE(TAG, "Error reading WAV header");
         fclose(play_file);
         vTaskDelete(NULL);
@@ -93,9 +96,11 @@ static void audio_task(void *arg)
     bsp_codec_set_fs(wav_header.sample_rate, wav_header.bits_per_sample, wav_header.num_channels);
 
     size_t bytes_sent = 0;
-    while (bytes_sent < wav_header.data_size) {
+    while (bytes_sent < wav_header.data_size)
+    {
         size_t bytes_read = fread(wav_bytes, 1, BUFFER_SIZE, play_file);
-        if (bytes_read <= 0) break;
+        if (bytes_read <= 0)
+            break;
         size_t bytes_written = 0;
         ESP_ERROR_CHECK(bsp_i2s_write(wav_bytes, bytes_read, &bytes_written, portMAX_DELAY));
         bytes_sent += bytes_written;
@@ -108,24 +113,66 @@ static void audio_task(void *arg)
     vTaskDelete(NULL); // Exit the task after playback
 }
 
-static void audio_init(void)
+void list_files_in_dir(const char *path)
 {
-    bsp_sdcard_init_default();  // Mount SD card
-
-    BaseType_t ret = xTaskCreate(audio_task, "audio_task", 4096, NULL, 6, NULL);
-    assert(ret == pdPASS);
+    struct dirent *dp;
+    DIR *dir = opendir(path);
+    if (dir == NULL)
+    {
+        ESP_LOGE(TAG, "Can't Open Dir.");
+    }
+    while ((dp = readdir(dir)) != NULL)
+    {
+        vTaskDelay(2000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "En el ciclo");
+        // ESP_LOGI(TAG, "dp: %s", dp);
+        ESP_LOGI(TAG, "[%s]\n", dp->d_name);
+    }
+    closedir(dir);
 }
 
-
-void app_main(void)
+static void app_init(void)
 {
     io_expander = bsp_io_expander_init();
     assert(io_expander != NULL);
     lvgl_disp = bsp_lvgl_init();
     assert(lvgl_disp != NULL);
-    audio_init();
-    
-    
+    // nvs key-value storage
+    bsp_sdcard_deinit_default();
+    if (bsp_sdcard_is_inserted())
+    {
+        esp_err_t ret = bsp_sdcard_init_default();
+        if (ret != ESP_OK)
+        {
+            ESP_LOGE(TAG, "SD Card initialization failed (0x%x). Audio playback disabled.", ret);
+            return;
+        }
+    }
+    bsp_rgb_init();
+    bsp_codec_init();
+    bsp_rtc_init();
+
+    bsp_codec_volume_set(100, NULL);
+}
+
+static void neutral_mood()
+{
+    if (lvgl_port_lock(0))
+    {
+        lv_obj_t *img = lv_img_create(lv_scr_act());
+        lv_img_set_src(img, &neutral_face);
+        lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+        // Release the mutex
+        lvgl_port_unlock();
+    }
+    BaseType_t task_ret = xTaskCreate(audio_task, "audio_task", 4096, NULL, 6, NULL);
+    assert(task_ret == pdPASS);
+}
+
+void app_main(void)
+{
+    app_init();
+
     // Lock the mutex due to the LVGL APIs are not thread-safe
     if (lvgl_port_lock(0))
     {
@@ -133,6 +180,8 @@ void app_main(void)
         lv_obj_t *img = lv_img_create(lv_scr_act());
         lv_img_set_src(img, &neutral_face);
         lv_obj_align(img, LV_ALIGN_CENTER, 0, 0);
+        BaseType_t task_ret = xTaskCreate(audio_task, "audio_task", 4096, NULL, 6, NULL);
+        assert(task_ret == pdPASS);
         // lv_demo_music(); /* A modern, smartphone-like music player demo. */
         // lv_demo_stress();       /* A stress test for LVGL. */
         // lv_demo_benchmark();    /* A demo to measure the performance of LVGL or to compare different settings. */
@@ -144,6 +193,9 @@ void app_main(void)
     while (1)
     {
         printf("free_heap_size = %ld\n", esp_get_free_heap_size());
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        // list_files_in_dir(DRV_BASE_PATH_SD);
+        // list_files_in_dir(DRV_BASE_PATH_FLASH);
+        neutral_mood();
+        vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
